7061895422659d2f92f9d8f633fa2707748ccc4a
unknown <sushaind96@gmail.com>
2024-07-28 11:25:03 +0530

    GRAIN-0001 | Testing a new commit

--- test-lambda-one/lambda_handler.py
+++ test-lambda-one/lambda_handler.py
@@ -2,6 +2,7 @@ import json
 
 def lambda_handler(event, context):
     print("woahhh")
+    print("Testing a new commit")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
